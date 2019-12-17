33c61c75f4dba5445bff1fd7401dab6d6c0e8b1e
Дмитрук Андрей <dmitruchok08@gmail.com>
2019-12-17 12:22:44 +0200

    .

--- js/task-1/js/script.js
+++ js/task-1/js/script.js
@@ -1,12 +1,30 @@
 function* fibonacci(num) {
-  let a = 0,
-      b = 1;
-  for (var i = 2; i <= num; i++) {
+  let c = true;
+
+if (c == false) {
+	let a = 0,
+    	b = 1;
+	for (var i = 2; i <= num; i++) {
       console.log (a); 
-      [a, b] = [b, a + b]
+      [a, b] = [b, a + b];
     }
+    console.log (a); 
     yield b; 
+    console.log (b); 
+} else if (c == true) {
+	let a = 0,
+    	b = 1;
+	for (var i = 2; i <= num; i++) {
+      console.log (a); 
+      [a, b] = [b, a - b];
+    }
+    console.log (a); 
+    yield b; 
+    console.log (b); 
+	}
 }
 
-let valueFib = fibonacci(prompt('Введите число'));
-  console.log (valueFib.next().value);
+	let valueFib = fibonacci(prompt('Введите число'));
+	let val = valueFib.next().value;
+		alert(val)
+ 		console.log (valueFib.next().value);
