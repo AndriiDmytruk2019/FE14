8d78cdc384e85556eab4bcf629a3db630993605e
Дмитрук Андрей <dmitruchok08@gmail.com>
2019-12-19 14:27:33 +0200

    .

--- js/task-1/js/script.js
+++ js/task-1/js/script.js
@@ -1,32 +1,57 @@
 function* fibonacci(num) {
+  let a = 0,
+      b = 1,
+      c = yield;
+  if (c == 'true') {
+    for (var i = 2; i <= num; i++) {
+      console.log (a); 
+      [a, b] = [b, a + b];
+    }
+    // yield b; 
+    console.log (b);
+    alert('Твое число ' + b +'!' + '  Заглини в консоль :)');
+} else if (c == 'false') {
+    for (var i = 2; i <= num; i++) {
+      console.log (a); 
+      [a, b] = [b, a - b];
+    }
+    // yield b; 
+    console.log (b);
+    alert('Твое число ' + b +'!' + '  Заглини в консоль :)');
+  };
+};
 
-if (c == 'true') {
-	let a = 0,
-    	b = 1;
 
-	for (var i = 2; i <= num; i++) {
+let valueFib = fibonacci(prompt('Введите число'));
+let guestion = prompt('Введите true для порядка вперёд и false - назад!'); 
+    console.log(valueFib.next(guestion));
+    console.log(valueFib.next(guestion));
+
+
+/*function* fibonacci(num) {
+    let a = 0,
+        b = 1,
+        c = yield;
+if (c == undefined) {
+	for (var i = 2; i <= num; i++) {yield b;
       console.log (a); 
       [a, b] = [b, a + b];
     }
     console.log (a); 
-    yield b; 
     console.log (b); 
-} else if (c == 'false') {
-	let a = 0,
-    	b = 1;
-
+} else if (c == true) {
 	for (var i = 2; i <= num; i++) {
       console.log (a); 
       [a, b] = [b, a - b];
     }
     console.log (a); 
-    yield b; 
     console.log (b); 
 	}
-}
+};
 
-let c = prompt('Введите true для порядка вперёд и false - назад!');
+// let c = prompt('Введите true для порядка вперёд и false - назад!');
 let valueFib = fibonacci(prompt('Введите число'));
-let val = valueFib.next().value;
-	alert('Твое число ' + val +'!' + '  Заглини в консоль :)')
-	console.log (valueFib.next().value);
+// let val = valueFib.next().value;
+  // valueFib.next(prompt('Введите'));
+// 	alert('Твое число ' + val +'!' + '  Заглини в консоль :)')
+*/
