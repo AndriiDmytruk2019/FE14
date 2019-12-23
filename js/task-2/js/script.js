ba362949e0ceb265240d4c7d0b368b578aadf698
Дмитрук Андрей <dmitruchok08@gmail.com>
2019-12-23 11:55:24 +0200

    .

--- js/task-2/js/script.js
+++ js/task-2/js/script.js
@@ -1,26 +1,28 @@
 function guessingGame(num) {
 
   let number = Number(num),
-      counter = 0,
+      counter = 1,
       min = 1,
-      max = 10,
+      max = 100,
+      diapazonv,
       randomNumber = getRandom(min, max);
 
   function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
   while (number !== randomNumber) {
     counter++;
-    console.log(number); console.log(randomNumber);console.log(counter);
-    number = +prompt(`Неверное число, попробуйте еще в новом диапазоне! Попытка № ${counter}`)
-  }
+    diapazon = number > randomNumber ? `${min}-${number}` : `${number}-${max}`;console.log(diapazon)
+    console.log(number); console.log(randomNumber);
+    number = +prompt(`Неверное число, попробуйте еще в новом диапазоне ${diapazon}! Попытка № ${counter}`);
+  };
 
   if ( number == randomNumber && counter > 0) {
     alert(`Вы угадали с ${counter} попитки)`)
   } else if (number == randomNumber && counter == 0 ) {
     alert(`Вы сразу угадалы, вы пророк!)`)
-  }
+  };
 };
 
 guessingGame(prompt('Угадай число от 0 до 100'));
