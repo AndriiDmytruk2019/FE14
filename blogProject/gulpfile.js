ddafa3bf329fd53ea59a12ae5689b209395e0ee5
Дмитрук Андрей <dmitruchok08@gmail.com>
2019-12-26 11:31:14 +0200

    .

--- blogProject/gulpfile.js
+++ blogProject/gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require( 'gulp' ),
+  watchSass = require( 'gulp-watch-sass' ),
 	sass = require( 'gulp-sass' ),
 	browserSync = require('browser-sync').create(),
 	autoprefixer = require( 'gulp-autoprefixer' ),
@@ -23,22 +24,20 @@ gulp.task('browser-sync', () => // Создаем таск browser-sync
 gulp.task('html', () =>
   gulp.src('app/*.html')
   .pipe(gulp.dest('dist'))
-  .pipe(browserSync.reload({stream: true}))
 );
 
 gulp.task( 'css', () => 
-	gulp.src( './app/css/**/*.css' )
+	gulp.src( 'app/css/**/*.css' )
   .pipe(gulp.dest('dist/css'))
-  .pipe(browserSync.reload({stream: true}))
 );
 
 gulp.task('scss', () => // Создаем таск Scss
-    gulp.src('./app/scss/**/*.scss') // Берем источник
-    .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass.
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-    .pipe(gulp.dest('./dist/css')) // Выгружаем результата в папку dist/css
-    .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+  gulp.src([ './app/**/*.scss', ]) // Берем источник
+   .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass.
+   .pipe(concat('style.min.css'))
+   .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+   .pipe(gulp.dest('dist/css')) // Выгружаем результата в папку dist/css
+   .pipe(browserSync.reload({stream: true}))
 );
 
 gulp.task('js', () => 
@@ -59,33 +58,16 @@ gulp.task('fonts', () =>
   .pipe(gulp.dest('./dist/fonts'))
 );
 
-gulp.task('sprite', () =>
-  gulp.src('./app/img/*.png')
-  .pipe(tasks.spritesmith({
-    imgName: 'sprite.png',
-    cssName: '_sprite.scss',
-    algoritm: 'left-right'
-  }))
-  .pipe(gulpif('*.png',gulp.dest('./dist/img/sprites')))
-  .pipe(gulpif('*.css', gulp.dest('./dist/scss/')))
-);
-
 gulp.task( 'clean', () => gulp.src( './dist/*', { read: false } )
 	.pipe( clean( ) )
 );
 
-gulp.task( 'watch', gulp.series('browser-sync', 'scss', 'css', 'html', 'js', 'img', 'fonts'),() => 
-  gulp.watch( './app/scss/**/*.scss', gulp.series( 'scss' )),
-  gulp.watch( './app/**/*.css', gulp.series( 'css' ) ),
-  gulp.watch( './app/**/*.js', gulp.series( 'js' ) ),
-  gulp.watch('./app/img/**/*.*',  gulp.series( 'img' )),
-  gulp.watch('./app/fonts/**/*.*', gulp.series( 'fonts' )),
-  gulp.watch('./app/*.html', gulp.series( 'html' ), browserSync.reload), // Наблюдение за HTML файлами в корне проекта
-  gulp.watch('./app/js/**/*.js', gulp.series( 'js' ), browserSync.reload), // Наблюдение за JS файлами в папке js
-);
+gulp.task( 'watch-scss', () => gulp.watch( './app/**/*.scss', gulp.series( 'scss' )));
+gulp.task( 'watch-css', () => gulp.watch('./app/**/*css', gulp.series( 'css' )));
+gulp.task( 'watch-img', () => gulp.watch('./app/img/**/*.*',  gulp.series( 'img' )));
+gulp.task( 'watch-fonts', () => gulp.watch('./app/fonts/**/*.*', gulp.series( 'fonts' )));
+gulp.task( 'watch-html', () => gulp.watch('./app/*.html', gulp.series( 'html' )));
+gulp.task( 'watch-js', () => gulp.watch('./app/js/**/*.js', gulp.series( 'js' )));
 
-gulp.task( 'build', gulp.series( 'clean', 'scss', 'css', 'html', 'js', 'img', 'fonts' ) );
+gulp.task( 'build', gulp.parallel( 'clean', 'html', 'scss', 'css', 'browser-sync', 'watch-scss', ) );
 
-gulp.task('clear', () =>
-  cache.clearAll()
-);
