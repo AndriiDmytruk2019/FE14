8afd0218a10cb96046b4eb080d53108a65b23c7d
Дмитрук Андрей <dmitruchok08@gmail.com>
2020-01-08 13:11:50 +0200

    .

--- blogProject/app/js/main.js
+++ blogProject/app/js/main.js
@@ -2,34 +2,34 @@ let blogList = [
     {
         topic: 'Спорт',
         title: 'Лучшая украинская теннисистка.',
-        date: '',
-        time: 'Mar 12 2012 12:00:00 AM',
+        time: 'March 02, 2019 00:20:18',
         page: 'Лучшая украинская теннисистка Элина Свитолина (№5 WTA) начала новый сезон с поражения на хардовом Премьере в Брисбене.'
     },
     {
         topic: 'Спорт',
         title: 'МЧМ по хоккею.',
-        date: '',
-        time: 'Mar 18 2012 10:00:00 AM',
+        time: 'July 21,, 2019 01:20:18',
         page: 'В финале Канада в напряженном матче обыграла Россию, хотя североамериканская сборная по ходу встречи проигрывала 0:1 и 1:3, однако сделала камбэк.'
     },
     {
         topic: 'Путешествие',
         title: 'Виды на Бали',
-        date: '',
-        time: 'Mar 12 2012 15:00:00 AM',
+        time: 'May 11, 2019 01:20:18',
         page: 'Лучшее место для жини!'
+    },
+    {
+        topic: 'Путешествие',
+        title: 'Лучшее где я был',
+        time: 'December 25, 2018 01:20:18',
+        page: 'Places'
     },
     {
         topic: 'Музыка',
         title: 'Смоки Мо представит «Стейкхаус»',
-        date: '',
-        time: '15:00:00 AM',
+        time: 'April 10, 2019 02:20:18',
         page: 'Концерт Смоки Мо состоится 4 апреля 2020 года в «Известия Hall». Рэпер представит композиции со своего недавнего альбома «Стейкхаус», а также лучшие песни с предыдущих восьми пластинок.'
     }
 ];
-
-const result = document.getElementById('result');
 
 let getValueHeading = function () {
     let indexSelect = document.getElementById( 'select_heading' ).options.selectedIndex;
@@ -38,30 +38,32 @@ let getValueHeading = function () {
     return value;
 };
 
-// let getValueTime = function () {
-//     let indexSelect = document.getElementById( 'select_time' ).options.selectedIndex;
-//     let select = document.getElementById( 'select_time' );
-//     let value = select.options[indexSelect].text;
-//     console.log(value)
-//     return value;
-// };
+let getValueTime = function () {
+    let indexSelect = document.getElementById( 'select_time' ).options.selectedIndex;
+    let select = document.getElementById( 'select_time' );
+    let value = select.options[indexSelect].text;
+    return value;
+};
 
-renderList( blogList,result );
-
-function filter(val,list){
+function filterVal(val,list){
     let result=[];
     list.forEach( i => {
     if(i.topic.indexOf(val) != -1)
         result.push(i)
-  })
+  });
     return result;
 };
 
-// console.log(new Date().getTime())
-let sortTime =  function (){
-    console.log( blogList.sort((a,b) => new Date(a.time).getTime()));
-    // return blogList.sort((a,b) => new Date(a.time).getTime() - new Date(b.time).getTime())
-};console.log(sortTime())
+let sortTime =  function (list, time){
+    if (time == 'Самые новые') {
+        return list.sort((a,b) => new Date(b.time).getTime() - new Date(a.time).getTime());
+    } else if (time == 'Давно написанные') {
+        return list.sort((a,b) => new Date(a.time).getTime() - new Date(b.time).getTime())
+    }
+};
+
+sortTime(blogList, getValueTime());
+renderList( blogList,result );
 
 function renderList(list,el){
     el.innerHTML='';
@@ -77,10 +79,25 @@ function renderList(list,el){
   })
 };
 
+
 document.getElementById( 'select_heading' ).addEventListener('change',e => {
     e.preventDefault();
-    let new_arr_heading = filter( getValueHeading(),blogList );
+    let new_arr_heading = filterVal( getValueHeading(),blogList );
     renderList( new_arr_heading,result );
+    if (getValueHeading() == 'Выберите жанр') {
+        renderList( blogList,result );
+    }
+});
+
+document.getElementById( 'select_time' ).addEventListener('change',e => {
+    e.preventDefault();
+    let new_arr_heading = filterVal( getValueHeading(),blogList );
+    sortTime(new_arr_heading, getValueTime())
+    renderList( new_arr_heading,result );
+    if (getValueHeading() == 'Выберите жанр') {
+        sortTime(blogList, getValueTime());
+        renderList( blogList,result );
+    }
 });
 
 
